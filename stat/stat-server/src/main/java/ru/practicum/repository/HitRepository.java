33c8ec5ad0b605b092d5522de134497ecb4da0e9
Misery-aripes <aaaa_wwww555@mail.ru>
2025-04-14 00:10:48 +0500

    Исправлены замечания ревью

--- stat/stat-server/src/main/java/ru/practicum/repository/HitRepository.java
+++ stat/stat-server/src/main/java/ru/practicum/repository/HitRepository.java
@@ -2,14 +2,12 @@ package ru.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import ru.practicum.model.EndpointHit;
 import ru.practicum.ViewStatsDto;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Repository
 public interface HitRepository extends JpaRepository<EndpointHit, Long> {
 
     @Query(" SELECT new ru.practicum.ViewStatsDto(eph.app, eph.uri, COUNT(eph.ip)) " +
